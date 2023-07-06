587d9c50d9746662b5230aa44f5d05fca7f4656d
190stl2022 <190@stl2022.info>
2023-07-06 12:57:55 +0800

    debug

--- validation.js
+++ validation.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const registerValidation = (data) => {
   const schema = Joi.object({
-    username: Joi.string().min(3).max(50).required(),
+    username: Joi.string().min(2).max(50).required(),
     email: Joi.string().min(6).max(50).required().email(),
     password: Joi.string().min(6).max(255).required(),
     role: Joi.string().required().valid('student', 'instructor')
